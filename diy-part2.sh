36d685a48bff4f8bec614058bbc77f6b07d8827c
qiu <qiuzhongqiang@users.noreply.github.com>
2022-11-09 17:32:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 ###### Modify hostname
 sed -i 's/OpenWrt/K2P-32M/g' package/base-files/files/bin/config_generate
@@ -20,4 +20,4 @@ sed -i 's/OpenWrt/K2P-32M/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt /Qiang build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' package/lean/default-settings/files/zzz-default-settings
 
 ####### Set argon as default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
