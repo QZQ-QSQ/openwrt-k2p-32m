69ccfe3b2a7559ffc2ec9fdd98496d916992bf3a
qiu <qiuzhongqiang@users.noreply.github.com>
2022-11-09 19:36:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >> 'feeds.conf.defaul'
+#echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>'feeds.conf.defaul'
 #clash备用链接
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> 'feeds.conf.default'
 ####### Replace with JerryKuKu’s Argon
