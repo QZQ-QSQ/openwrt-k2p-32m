9452d5052a38cf93626d04e18b26d16f6f2fbcb4
qiu <qiuzhongqiang@users.noreply.github.com>
2022-11-16 15:59:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,29 +10,27 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
+##### Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add Custom feed source
+##### Add Custom feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>'feeds.conf.defaul'
-#clash备用链接
-
+##### clash备用链接
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> 'feeds.conf.default'
-####### Replace with JerryKuKu’s Argon
+##### Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf #delete original argon theme
 #echo 'src-git ArgonTheme https://github.com/jerrykuku/luci-theme-argon.git' >> 'feeds.conf.default' #repleace with JerryKuKu’s Argon Theme
 #echo 'src-git ArgonThemeConfig https://github.com/jerrykuku/luci-app-argon-config.git' >> 'feeds.conf.default' #add JerryKuKu’s Argon Config Feature
 #cd package/lean  
-#rm -rf luci-theme-argon  #删除原Argon主题
+##### rm -rf luci-theme-argon  #删除原Argon主题
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  #加入JerryKuKu’s Argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config #加入JerryKuKu’s Argon Config
 #cd ..
 #cd ..
 
-###### K2P-32M修改编译文件 ######
+##### K2P-32M修改编译文件 ######
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
