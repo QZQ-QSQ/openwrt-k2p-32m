ac07c4d6ccc14ae7139ce4e94fbf566f8a8a394a
qiu <qiuzhongqiang@users.noreply.github.com>
2022-11-16 15:57:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,9 +33,7 @@ echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> 'feeds.co
 #cd ..
 
 ###### K2P-32M修改编译文件 ######
-
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
