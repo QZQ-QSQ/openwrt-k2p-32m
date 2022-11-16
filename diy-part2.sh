3e6cfbfcc84436beccb9eba62bc308278d0e7b66
qiu <qiuzhongqiang@users.noreply.github.com>
2022-11-16 16:01:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,16 +8,11 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
-
-###### Modify hostname
-#sed -i 's/OpenWrt/K2P-32M/g' package/base-files/files/bin/config_generate
-
-####### Modify the version number
-#sed -i 's/OpenWrt /Qiang build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' package/lean/default-settings/files/zzz-default-settings
-
-####### Set argon as default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+##### Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+##### Modify hostname
+sed -i 's/OpenWrt/K2P-32M/g' package/base-files/files/bin/config_generate
+##### Modify the version number
+sed -i 's/OpenWrt /Qiang build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' package/lean/default-settings/files/zzz-default-settings
+##### Set argon as default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
